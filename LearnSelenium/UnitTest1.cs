f5251e631f5aef5c8a4245e7ffa0beb81cff8480
stefanrammo <86823284+stefanrammo@users.noreply.github.com>
2024-10-14 10:46:54 +0300

    Added Assertions

--- LearnSelenium/UnitTest1.cs
+++ LearnSelenium/UnitTest1.cs
@@ -2,15 +2,10 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using OpenQA.Selenium.Support.UI;
-using System.Collections.Generic;
-using OpenQA.Selenium.Interactions;
-using System.Collections.ObjectModel;
 using System.Threading;
 
-
 namespace LearnSelenium
-    {
+{
     [TestFixture]
     public class Tests
     {
@@ -25,61 +20,73 @@ namespace LearnSelenium
         }
 
         [Test]
-        public void TestPage1()
+        public void TestPage1()  // Navigates to the Blog page
         {
-            driver.Url = test_url;
-            driver.Navigate().GoToUrl("https://stefanrammo23.thkit.ee/");
+            driver.Navigate().GoToUrl(test_url);
 
-            try { IWebElement sButton2 = driver.FindElement(By.XPath("//*[contains(@class, 'menu-item-1272')]")); sButton2.Click(); } catch (Exception) { }
+            IWebElement sButton2 = driver.FindElement(By.XPath("//*[contains(@class, 'menu-item-1272')]"));
+            sButton2.Click();
+
             Thread.Sleep(2500);
+
+            IWebElement blogHeading = driver.FindElement(By.XPath("//h1[contains(text(), 'Blog')]"));
+
+            Assert.IsTrue(blogHeading.Displayed, "TestPage1 failed: Blog page did not load successfully.");
         }
 
         [Test]
-        public void TestPage2()
+        public void TestPage2()  // Navigates to the About page
         {
-            driver.Url = test_url;
-            driver.Navigate().GoToUrl("https://stefanrammo23.thkit.ee/");
+            driver.Navigate().GoToUrl(test_url);
 
-            try { IWebElement sButton2 = driver.FindElement(By.XPath("//*[@id=\"menu-item-12\"]")); sButton2.Click(); } catch (Exception) { }
+            IWebElement sButton2 = driver.FindElement(By.XPath("//*[@id=\"menu-item-12\"]"));
+            sButton2.Click();
+
             Thread.Sleep(2500);
+
+            IWebElement aboutHeading = driver.FindElement(By.XPath("//h1[contains(text(), 'Who We Are?')]"));
+
+            Assert.IsTrue(aboutHeading.Displayed, "TestPage2 failed: About page did not load successfully.");
         }
+
 
         [Test]
-        public void TestPage3()
+        public void TestPage3() //Navigate to Contact and try to Submit a valid form
         {
-            driver.Url = test_url;
-            driver.Navigate().GoToUrl("https://stefanrammo23.thkit.ee/");
+            driver.Navigate().GoToUrl(test_url);
 
-            try { IWebElement sButton2 = driver.FindElement(By.XPath("//*[contains(@class, 'menu-item-150')]")); sButton2.Click(); } catch (Exception) { }
+            IWebElement sButton2 = driver.FindElement(By.XPath("//*[contains(@class, 'menu-item-150')]"));
+            sButton2.Click();
             Thread.Sleep(2500);
 
-            var sText = driver.FindElements(By.XPath("//*[@id=\"wpforms-7-field_0\"]"));
-            for (int i = 0; i < sText.Count; i++)
-            {
-                try { sText[i].Click(); sText[i].SendKeys("Stefan Rammo"); } catch (Exception) { }
-            }
+            var sText = driver.FindElement(By.XPath("//*[@id=\"wpforms-7-field_0\"]"));
+            sText.Click();
+            sText.SendKeys("Stefan Rammo");
+
+            var sTextArea = driver.FindElement(By.XPath("//*[@id=\"wpforms-7-field_1\"]"));
+            sTextArea.Click();
+            sTextArea.SendKeys("stefanrammo@email.co.uk");
+
+            var sTextArea1 = driver.FindElement(By.XPath("//*[@id=\"wpforms-7-field_2\"]"));
+            sTextArea1.Click();
+            sTextArea1.SendKeys("This is a Selenium test");
+
             Thread.Sleep(2500);
-            var sTextArea = driver.FindElements(By.XPath("//*[@id=\"wpforms-7-field_1\"]"));
-            for (int i = 0; i < sTextArea.Count; i++)
-            {
-                try { sTextArea[i].Click(); sTextArea[i].SendKeys("stefanrammo@email.co.uk"); } catch (Exception) { }
-            }
-            Thread.Sleep(2500);
-            var sTextArea1 = driver.FindElements(By.XPath("//*[@id=\"wpforms-7-field_2\"]"));
-            for (int i = 0; i < sTextArea1.Count; i++)
-            {
-                try { sTextArea1[i].Click(); sTextArea1[i].SendKeys("This is a Selenium test"); } catch (Exception) { }
-            }
-            Thread.Sleep(2500);
-            try { IWebElement sButton2 = driver.FindElement(By.XPath("//*[@id=\"wpforms-submit-7\"]")); sButton2.Click(); } catch (Exception) { }
-            Thread.Sleep(2500);
+            IWebElement submitButton = driver.FindElement(By.XPath("//*[@id=\"wpforms-submit-7\"]"));
+            submitButton.Click();
+
+            Thread.Sleep(2500); 
+            IWebElement confirmationMessage = driver.FindElement(By.XPath("//*[contains(text(), 'Thanks for contacting us! We will be in touch with you shortly.')]"));
+
+            Assert.IsTrue(confirmationMessage.Displayed, "TestPage3 failed: Form submission did not complete successfully.");
         }
 
-        [TearDownAttribute]
-        public void TearDownAttribute()
+
+        [TearDown]
+        public void TearDown()
         {
-            driver.Quit();
             driver.Dispose();
+            driver.Quit();
         }
     }
 }
